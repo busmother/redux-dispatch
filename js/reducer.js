e93067d78e048b5d5cba8eacb63cc326d67466e4
Ian Marshall <imarshall14@gmail.com>
2021-05-27 15:12:20 -0400

    commit tree b68d037abddbc3773cffa7e84162ce3d9fbfdad3

--- js/reducer.js
+++ js/reducer.js
@@ -1,4 +1,29 @@
-function changeState(state, action){
+// let state;
+
+// function changeState(state = {count:0}, action){
+//     switch (action.type) {
+//       case 'INCREASE_COUNT':
+//         return {count: state.count + 1}
+//       default:
+//         return state;
+//     }
+//   }
+
+// function dispatch(action){
+//   state = changeState(state, action)
+//   render()
+// }
+
+// function render(){
+//     document.body.textContent = state.count
+// }
+
+// dispatch({type: '@@INIT'}) 
+
+
+let state;
+
+function reducer(state = {count: 0}, action){
   switch (action.type) {
     case 'INCREASE_COUNT':
       return {count: state.count + 1}
@@ -7,7 +32,14 @@ function changeState(state, action){
   }
 }
 
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
+function dispatch(action){
+  state = reducer(state, action)
+  render()
+}
 
-changeState(state, action)
+function render(){
+  let container = document.getElementById('container');
+  container.textContent = state.count;
+}
+
+dispatch({type: '@@INIT'}) 
